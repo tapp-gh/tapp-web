87eef3249de1cd2327f6d65d0c464741448514c6
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-02-25 03:31:51 -0500

    validation

--- src/components/LogInForm.tsx
+++ src/components/LogInForm.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React from 'react'
 import { useForm } from 'react-hook-form';
 import {Link} from 'react-router-dom'
 import { login } from '../http/auth';
