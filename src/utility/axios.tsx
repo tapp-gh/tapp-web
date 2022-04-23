f98344f5e6329a1147610e9149b66fe7e03f8340
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-04-23 15:23:35 +0000

    redux installation

--- src/utility/axios.tsx
+++ src/utility/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://tapp-api-0.herokuapp.com/api/v1/",
+    baseURL: "https://2e1a-154-160-7-129.eu.ngrok.io/api/v1/",
     headers: {
       "Content-Type": "application/json",
     },
